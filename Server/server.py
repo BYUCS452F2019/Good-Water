57bf866133046f7a8311b7c5507fbcd367603bbd
Jeffrey Woodbury <jeffreywayne96@gmail.com>
2019-10-25 09:51:09 -0600

    sample Server

--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,17 @@
+import http.server
+import socketserver
+
 class Server():
     def __init__(self):
         pass
+    def start(self):
+        PORT = 8080
+        Handler = http.server.SimpleHTTPRequestHandler
+
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            print("serving at port", PORT)
+            httpd.serve_forever()
+
 
     #This class is responsible for making API endpoints that
     # will receive JSON objects and then call DAO methods such as "addUser"
