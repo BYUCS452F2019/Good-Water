f77fc0601d7e7373f9b5f33f82e257147b1ff0b5
Spencer Young <spencerwyoung@outlook.com>
2019-12-04 13:23:06 -0700

    implement rest of MongoDAO. Untested but ¯\_(ツ)_/¯

--- Server/dao/mongodao.py
+++ Server/dao/mongodao.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Any, Optional
 
+from bson import ObjectId
 from pymongo import MongoClient
 from pymongo.database import Collection, Database
 
@@ -17,6 +18,14 @@ class MongoDAO:
         self.host = host
         self.port = port
         self.db_name = db_name
+
+    def _get_document_id(
+            self,
+            collection: Collection,
+            params: Dict[str, Any],
+    ) -> Optional[str]:
+        doc = collection.find_one(params)
+        return str(doc["_id"]) if doc is not None else None
 
     def connect_to_database(self):
         self.client = MongoClient(
@@ -35,10 +44,11 @@ class MongoDAO:
         buildings: Collection = self.db.buildings
         campuses: Collection = self.db.campuses
 
-        users.create_index("UserName", unique=True)
-        fountains.create_index("Name", unique=True)
-        buildings.create_index("Name", unique=True)
-        campuses.create_index("Name", unique=True)
+        users.create_index("userName", unique=True)
+        fountains.create_index("name", unique=True)
+        fountains.create_index("buildingID")
+        buildings.create_index("name", unique=True)
+        campuses.create_index("name", unique=True)
 
     def add_user(
         self,
@@ -52,10 +62,10 @@ class MongoDAO:
         """
         users: Collection = self.db.users
         users.insert_one({
-            "UserName": user_name,
-            "FirstName": first_name,
-            "LastName": last_name,
-            "Password": password,
+            "userName": user_name,
+            "firstName": first_name,
+            "lastName": last_name,
+            "password": password,
         })
 
     def login_user(
@@ -69,7 +79,7 @@ class MongoDAO:
         """
         users: Collection = self.db.users
         user = users.find_one({
-            "UserName": user_name
+            "userName": user_name
         })
 
         if user is None:
@@ -100,13 +110,25 @@ class MongoDAO:
             building_id: str,
             fountain_name: str,
     ):
-        raise NotImplementedError()
+        fountains: Collection = self.db.fountains
+        fountains.insert_one({
+            "name": fountain_name,
+            "buildingID": ObjectId(building_id),
+        })
 
     def lookup_fountain(
             self,
             fountain_name: str,
     ) -> Optional[Dict[str, Any]]:
-        raise NotImplementedError()
+        fountains: Collection = self.db.fountains
+        fountain = fountains.find_one({
+            "name": fountain_name,
+        })
+
+        if fountain is None:
+            return None
+
+        return str(fountain["_id"])
 
     def add_building(
             self,
@@ -115,7 +137,13 @@ class MongoDAO:
             longitude: float,
             campus_id: str,
     ):
-        raise NotImplementedError()
+        buildings: Collection = self.db.buildings
+        buildings.insert_one({
+            "name": building_name,
+            "latitude": latitude,
+            "longitude": longitude,
+            "campus_id": ObjectId(campus_id),
+        })
 
     def add_campus(
             self,
@@ -123,22 +151,72 @@ class MongoDAO:
             state: str,
             campus_name: str,
     ):
-        raise NotImplementedError()
+        campuses: Collection = self.db.campuses
+        campuses.insert_one({
+            "name": campus_name,
+            "city": city,
+            "state": state,
+        })
 
     def get_campus_id(
             self,
             campus_name: str
     ) -> Optional[str]:
-        raise NotImplementedError()
+        return self._get_document_id(
+            self.db.campuses,
+            {"name": campus_name},
+        )
 
     def list_fountains(
             self,
             building_name: str,
     ) -> List[Dict[str, Any]]:
-        raise NotImplementedError()
+        building_id = self._get_document_id(
+            self.db.buildings,
+            {"name": building_name},
+        )
+
+        if building_id is None:
+            # TOOD: should this be an error?
+            return []
+
+        fountains: Collection = self.db.fountains
+        fountain_docs = fountains.find({
+            "buildingID": ObjectId(building_id),
+        })
+
+        return [
+            {
+                "id": str(f["_id"]),
+                "name": f["name"],
+            }
+            for f in fountain_docs
+        ]
 
     def list_buildings(
             self,
             campus_name: str,
     ) -> List[Dict[str, Any]]:
-        raise NotImplementedError()
+        campus_id = self._get_document_id(
+            self.db.campuses,
+            {"name": campus_name},
+        )
+
+        if campus_id is None:
+            # TODO: should this be an error?
+            return []
+
+        buildings: Collection = self.db.buildings
+        building_docs = buildings.find({
+            "campusID": ObjectId(campus_id),
+        })
+
+        return [
+            {
+                "id": str(b["_id"]),
+                "name": b["name"],
+                "latitude": b["latitude"],
+                "longitude": b["longitude"],
+            }
+            for b in building_docs
+        ]
